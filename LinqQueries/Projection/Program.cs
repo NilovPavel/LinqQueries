e58c2c3841dcb71c876a4ebc8151c568d76966ea
NilovPavel <nilowpavel@gmail.com>
2024-10-01 06:49:43 +0300

    Start of zip-description.

--- LinqQueries/Projection/Program.cs
+++ LinqQueries/Projection/Program.cs
@@ -25,5 +25,8 @@ foreach (string s in query)
     Console.WriteLine(s);
 }
 
-//Разобраться
-//phrases.SelectMany()
+//Zip
+// An int array with 7 elements.
+IEnumerable<int> numbers = [1, 2, 3, 4, 5, 6, 7];
+// A char array with 6 elements.
+IEnumerable<char> letters = ['A', 'B', 'C', 'D', 'E', 'F'];
